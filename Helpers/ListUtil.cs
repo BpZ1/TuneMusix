51c5584e82c92c95e06efc6625571cfa4c9c5349
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-05-18 04:31:50 +0200

    -Added method for randomizing lists

--- Helpers/ListUtil.cs
+++ Helpers/ListUtil.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,6 +21,28 @@ namespace TuneMusix.Helpers
             for (int i = 0; i < list.Count; i += size)
             {
                 yield return list.GetRange(i, Math.Min(size, list.Count - i));
+            }
+        }
+
+        /// <summary>
+        /// Shuffles a given list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list"></param>
+        public static void Shuffle<T>(this IList<T> list)
+        {
+            var provider = new RNGCryptoServiceProvider();
+            int n = list.Count;
+            while (n > 1)
+            {
+                var box = new byte[1];
+                do provider.GetBytes(box);
+                while (!(box[0] < n * (Byte.MaxValue / n)));
+                var k = (box[0] % n);
+                n--;
+                var value = list[k];
+                list[k] = list[n];
+                list[n] = value;
             }
         }
     }
