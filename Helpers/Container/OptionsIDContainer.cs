f14f9662683ad848bf82ec88f4f8a4f2f3cf489a
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-05-18 04:31:09 +0200

    -Added container class for options and ID

---
+++ Helpers/Container/OptionsIDContainer.cs
@@ -0,0 +1,16 @@
+﻿using TuneMusix.Model;
+
+namespace TuneMusix.Helpers.Container
+{
+    public class OptionsIDContainer
+    {
+        public Options options;
+        public long id;
+
+        public OptionsIDContainer(Options options, long id)
+        {
+            this.options = options;
+            this.id = id;
+        }
+    }
+}
