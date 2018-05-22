0b442cbc3f22f6de3b82840792d66007e83f3fa6
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-05-22 23:15:03 +0200

    Changed closing method to include all cases

--- ViewModel/OptionsViewModel/OptionsOverviewViewModel.cs
+++ ViewModel/OptionsViewModel/OptionsOverviewViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,14 @@ namespace TuneMusix.ViewModel.OptionsViewModel
         private Options options = Options.Instance;
         private DataModel dataModel = DataModel.Instance;
 
+
+        public RelayCommand ExitOptionsWindow { get; set; }
         public RelayCommand Apply { get; set; }
         public RelayCommand Cancel { get; set; }
 
         public OptionsOverviewViewModel()
         {
+            ExitOptionsWindow = new RelayCommand(exitOptionsWindow);
             Apply = new RelayCommand(apply);
             Cancel = new RelayCommand(cancel);
         }
@@ -44,41 +48,56 @@ namespace TuneMusix.ViewModel.OptionsViewModel
         private void cancel(object argument)
         {
             OptionsWindowView optionsWindow = argument as OptionsWindowView;
+            optionsWindow.Close();
+        }
+        /// <summary>
+        /// Gets called when the window is closed.
+        /// </summary>
+        /// <param name="argument"></param>
+        private void exitOptionsWindow(object argument)
+        {
+            var eventArgs = argument as CancelEventArgs;
+
             if (options.IsModified())
             {
                 //Open a confirmation dialog to check if the user wants to save his changes.
-                DialogResult result = openDialog();
-       
-                if(result == DialogResult.Yes)
+                DialogResult result = openDialog("Do you want to save the changes?");
+
+                if (result == DialogResult.Yes)
                 {
                     options.Save();
-                    optionsWindow.Close();
+                    eventArgs.Cancel = false;
                 }
-                else if(result == DialogResult.No)
+                else if (result == DialogResult.No)
                 {
-                    Console.Out.WriteLine("NO was pressed");
                     //Restoring previous settings
                     SQLLoader loader = new SQLLoader();
                     loader.LoadOptions();
-                    optionsWindow.Close();
+                    eventArgs.Cancel = false;
                 }
-                else if(result == DialogResult.Undefined)
+                else if (result == DialogResult.Undefined)
                 {
-
+                    eventArgs.Cancel = true;
                 }
             }
             else
             {
-                optionsWindow.Close();
+                eventArgs.Cancel = false;
             }
         }
-
-        private DialogResult openDialog()
+        /// <summary>
+        /// Opens a window and asks the user for confirmation.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        private DialogResult openDialog(string message)
         {
-            DialogViewModelBase vm = new ConfirmationDialogViewModel();
+            DialogViewModelBase vm = new ConfirmationDialogViewModel(message);
             DialogResult result = DialogService.OpenDialog(vm);
             return result;
         }
+
+
 
     }
 }
