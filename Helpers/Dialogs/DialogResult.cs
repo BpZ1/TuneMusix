58add15c77f468f321a59360ade5c9c02ffa850d
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-05-22 23:12:34 +0200

    Added documentation

--- Helpers/Dialogs/DialogResult.cs
+++ Helpers/Dialogs/DialogResult.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace TuneMusix.Helpers.Dialogs
 {
+    /// <summary>
+    /// Enum containing the results of a confirmation dialog.
+    /// </summary>
     public enum DialogResult
     {
         Yes,
