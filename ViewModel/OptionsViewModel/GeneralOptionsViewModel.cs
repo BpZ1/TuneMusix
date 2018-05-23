dcec5caac9f42cba827c2c3b2b2f69a39f8f8768
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-05-24 01:59:49 +0200

    Added property for confirmation.

--- ViewModel/OptionsViewModel/GeneralOptionsViewModel.cs
+++ ViewModel/OptionsViewModel/GeneralOptionsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,17 +8,36 @@ using TuneMusix.Model;
 
 namespace TuneMusix.ViewModel.OptionsViewModel
 {
-    class GeneralOptionsViewModel
+    class GeneralOptionsViewModel : INotifyPropertyChanged
     {
         private Options options = Options.Instance;
+
+
         public bool LoggerActive
         {
             get { return options.LoggerActive; }
             set
             {
                 options.LoggerActive = value;
-                options.Modified = true;
+                RaisePropertyChanged("LoggerActive");
             }
         }
+
+        public bool AskConfirmation
+        {
+            get { return options.AskConfirmation; }
+            set
+            {
+                options.AskConfirmation = value;
+                RaisePropertyChanged("AskConfirmation");
+            }
+        }
+
+        internal void RaisePropertyChanged(string prop)
+        {
+            if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(prop)); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
