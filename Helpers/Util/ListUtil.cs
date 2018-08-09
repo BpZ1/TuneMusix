f7b3fb0b9e3f9987181fc71b1f25df12df19105b
Nicolas Bruch <nicolas-emil.bruch@stud.uni-bamberg.de>
2018-08-09 12:38:37 +0200

    Removed unused imports.

--- Helpers/Util/ListUtil.cs
+++ Helpers/Util/ListUtil.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-using TuneMusix.Helpers.Dialogs;
-using TuneMusix.ViewModel.Dialog;
 
 namespace TuneMusix.Helpers
 {
